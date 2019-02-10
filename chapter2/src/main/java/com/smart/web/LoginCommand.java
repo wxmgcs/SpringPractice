bb2d2a96e1ec65d211538fb11e96ea8d56a87409
Patrick <wxmgcs@sina.cn>
2019-02-10 18:22:08 +0800

    启动程序

--- chapter2/src/main/java/com/smart/web/LoginCommand.java
+++ chapter2/src/main/java/com/smart/web/LoginCommand.java
@@ -1,23 +1,23 @@
 package com.smart.web;
 
 public class LoginCommand {
-	private String userName;
+    private String userName;
 
-	private String password;
+    private String password;
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-	public String getUserName() {
-		return userName;
-	}
+    public String getUserName() {
+        return userName;
+    }
 
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
--- chapter2/src/main/java/com/smart/web/LoginController.java
+++ chapter2/src/main/java/com/smart/web/LoginController.java
@@ -15,33 +15,33 @@ import com.smart.domain.User;
 import com.smart.service.UserService;
 
 @RestController
-public class LoginController{
-	private UserService userService;
-    
-	@RequestMapping(value = "/index.html")
-	public String loginPage(){
-		return "login";
-	}
-	
-	@RequestMapping(value = "/loginCheck.html")
-	public ModelAndView loginCheck(HttpServletRequest request,LoginCommand loginCommand){
-		boolean isValidUser =  userService.hasMatchUser(loginCommand.getUserName(),
-					                    loginCommand.getPassword());
-		if (!isValidUser) {
-			return new ModelAndView("login", "error", "用户名或密码错误。");
-		} else {
-			User user = userService.findUserByUserName(loginCommand
-					.getUserName());
-			user.setLastIp(request.getLocalAddr());
-			user.setLastVisit(new Date());
-			userService.loginSuccess(user);
-			request.getSession().setAttribute("user", user);
-			return new ModelAndView("main");
-		}
-	}
+public class LoginController {
+    private UserService userService;
 
-	@Autowired
-	public void setUserService(UserService userService) {
-		this.userService = userService;
-	}
+    @RequestMapping(value = "/index.html")
+    public String loginPage() {
+        return "login";
+    }
+
+    @RequestMapping(value = "/loginCheck.html")
+    public ModelAndView loginCheck(HttpServletRequest request, LoginCommand loginCommand) {
+        boolean isValidUser = userService.hasMatchUser(loginCommand.getUserName(),
+                loginCommand.getPassword());
+        if (!isValidUser) {
+            return new ModelAndView("login", "error", "用户名或密码错误。");
+        } else {
+            User user = userService.findUserByUserName(loginCommand
+                    .getUserName());
+            user.setLastIp(request.getLocalAddr());
+            user.setLastVisit(new Date());
+            userService.loginSuccess(user);
+            request.getSession().setAttribute("user", user);
+            return new ModelAndView("main");
+        }
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
 }
